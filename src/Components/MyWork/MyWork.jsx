c6922d2834ad5d21608979e8a4ae24c6a2ef9cb7
Yashbharadva <yashbharadva03@gmail.com>
2025-04-21 19:48:38 +1200

    my services

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Mywork.css';
+import theme_pattern from "../../assets/theme_pattern.svg";
+
+
+const MyWork = () => {
+    return (
+        <div className="mywork">
+            <div className="mywork-title">
+                <h1>My latest work</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+        </div>
+    )
+}
+
+
+export default MyWork;  
